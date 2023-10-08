f1172d67756b1a6eb6a9a21077822a8c813efe8a
shashikalagit config --global user.email <padskala2015@gmail.com>
2023-10-08 13:51:41 +0530

    signup

--- src/components/LOGIN/Login.jsx
+++ src/components/LOGIN/Login.jsx
@@ -1,4 +1,4 @@
-import React, { useRef,useState } from 'react'
+import React, {useState } from 'react'
 import './Login.css'
 import Facebook from '../../Logo/facebook.png'
 import Google from '../../Logo/Google.png'
@@ -13,19 +13,33 @@ const Login = ({setLoginDetails}) => {
    
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState(" ");
-    // const email = useRef();
-    // const password = useRef();
-  
+    
     const LoginData = (e) => {
         e.preventDefault();
 
         // console.log("shashikala");
         // console.log(email,password );
+        function ValidateEmail(inputText)
+{
+var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+if(inputText.value.match(mailformat))
+{
+alert("Valid email address!");
+document.form1.text1.focus();
+return true;
+}
+else
+{
+alert("You have entered an invalid email address!");
+document.form1.text1.focus();
+return false;
+}
+}
+
         
-        if (email ===localStorage.getItem("email","abc@gmail.com") && password === localStorage.getItem("password","12345")) {
+        if (email ===localStorage.getItem("email") && password === localStorage.getItem("password")) {
             // if(email.current.value==="abc@gmail.com"&&password.current.value==="12345"){
-            //     localStorage.setItem("emailData","abc@gmail.com");
-            //     localStorage.setItem("passwordData","12345");
+         
 
             let obj = [{
                 userName: localStorage.getItem("userName"),
@@ -35,6 +49,10 @@ const Login = ({setLoginDetails}) => {
             setLoginDetails(obj)
             alert("Login Successfully");
             navegate('/')
+        }
+        else{
+            alert("You have entered an invalid email address!");
+             
         }
 
     }
@@ -78,3 +96,73 @@ const Login = ({setLoginDetails}) => {
 }
 
 export default Login
+// const Login = ({setLoginDetails}) => {
+//     const navegate = useNavigate();
+//     // const localContext = useContext(DataParentContext);
+//     // const { LoginDetails, setLoginDetails } = localContext;
+   
+//     const [email, setEmail] = useState("");
+//     const [password, setPassword] = useState(" ");
+    
+//     const LoginData = (e) => {
+//         e.preventDefault();
+
+//         // console.log("shashikala");
+//         // console.log(email,password );
+        
+//         if (email ===localStorage.getItem("email") && password === localStorage.getItem("password")) {
+//             // if(email.current.value==="abc@gmail.com"&&password.current.value==="12345"){
+         
+
+//             let obj = [{
+//                 userName: localStorage.getItem("userName"),
+//                 email: localStorage.getItem("email"),
+//                 password: localStorage.getItem("password")
+//             }]
+//             setLoginDetails(obj)
+//             alert("Login Successfully");
+//             navegate('/')
+//         }
+
+//     }
+//     return (      
+    
+//         <form>
+//             <div className='LoginContainer'>
+//                 <div className='mainLogin'>
+//                     <h1 style={{ textAlign: "center", marginTop: "15px" }}>Log in</h1>
+//                     <h3 style={{ textAlign: "center", marginTop: "-15px" }}>Don't have an account?<span style={{ color: "rgb(13,110,253)" }}><Link to="/signUp" style={{ color: "rgb(13,110,253)", textDecoration: "none" }} > Sign Up </Link></span></h3>
+//                     <div className='icons'>
+//                         <button style={{ marginRight: "10px" }}><img src={Google} alt="" style={{ width: "38px", borderRadius: "50%" }} /> Sign in with <span style={{ color: "red", }}><b>Google</b></span></button>
+//                         <button id='iconFace'><img src={Facebook} alt="" /></button>
+//                     </div>
+//                     <div className='OR'>
+//                         <div className='spaceDiv'></div>
+//                         <span className='OORR'>or</span>
+//                         <div className='spaceDiv'></div>
+//                     </div>
+//                     <div className='center'>
+//                         <div className='Email'>
+//                             <label htmlFor="" style={{ marginRight: "220px" }}>Email</label><br />
+//                             <input type="text"  placeholder='Enter Email' 
+//                             onChange={(e) => setEmail(e.target.value)}
+//                              required /><br />
+//                         </div>
+//                         <div className='Email'>
+//                             <label htmlFor="" style={{ marginRight: "200px" }}>Password</label><br />
+//                             <input type="password"  placeholder='Enter Password' 
+//                              onChange={(e) => setPassword(e.target.value)}
+//                              required  /><br />
+//                         </div>
+//                         <button onClick={(e)=>LoginData(e)}>Log in</button>
+//                         <p style={{ color: "rgb(13,110,253)", cursor: "pointer" }}>Forgot Password ?</p>
+//                     </div>
+//                 </div>
+//             </div>
+//         </form>
+        
+//     );
+// }
+
+// export default Login
+
